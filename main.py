d6ad0bc07ffaf9472bc9c6788a0ac154e244f6ce
Glenn Song <glennsong09@gmail.com>
2026-02-23 16:42:24 -0600

    Fix error

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import discord
 import os
 from discord.ext import commands
-from cogs.quests import Quests
+from discord import Intents
 from database import db
 from dotenv import load_dotenv
 
 db.build()
 load_dotenv()
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix="!", intents = intents)
 token = os.getenv("TOKEN")
 
-@bot.command()
-async def load (ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
+''' @bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f'cogs.{extension}')
 
 @bot.command()
-async def unload (ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+async def unload(ctx, extension):
+    await bot.unload_extension(f'cogs.{extension}') ''' 
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+async def setup_hook():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.setup_hook = setup_hook
 bot.run(token)
