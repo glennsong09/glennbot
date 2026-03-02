8eeeb4b44b4ecf57624f232dc7ddb82f886649ce
Glenn Song <glennsong09@gmail.com>
2026-03-02 13:39:20 -0600

    Fix

--- cogs/riftbound.py
+++ cogs/riftbound.py
@@ -123,10 +123,11 @@ async def make_pack(type):
         if c["metadata"]["overnumbered"] == True and c["metadata"]["signature"] == False
     ]
 
-    runes = [c for c in set_cards if c["classification"]["type"] == "Rune" 
+    runes = [c for c in all_cards if c["classification"]["type"] == "Rune" 
                 and c["classification"]["rarity"] == "Common"]
-    tokens = [c for c in set_cards if (c["classification"]["type"] == "Token")]
-    alt_art_runes = [c for c in showcases if c["classification"]["type"] == "Rune"]
+    tokens = [c for c in all_cards if (c["classification"]["type"] == "Token")]
+    alt_art_runes = [c for c in all_cards if ((c["classification"]["type"] == "Rune")
+                        and (c["metadata"]["alternate_art"] == True))]
 
     cards = []
 
